00556459cff443f990d314811cee4d0fd70bc407
satot <ph1.ph2.sa25@gmail.com>
2017-09-11 02:34:53 +0800

    Show eta on header (not realtime yet)

--- app/models/route.rb
+++ app/models/route.rb
@@ -2,10 +2,19 @@ class Route < ApplicationRecord
   has_many :steps
   accepts_nested_attributes_for :steps, allow_destroy: true
   scope :time_desc, ->{order(created_at: :desc)}
+  MINUTE = 60
 
   self.inheritance_column = nil
   enum type: {
     recent: 1,
     saved: 2
   }
+
+  def duration
+    steps.map{|s| s.body["duration"]["value"]}.inject(&:+).to_f / MINUTE
+  end
+
+  def eta
+    Time.now() + duration.minutes
+  end
 end
